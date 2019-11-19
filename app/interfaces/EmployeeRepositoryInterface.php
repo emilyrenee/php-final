474bd99ddea9990b137b2d0323b53a5dd61a12c5
emilyrenee <emily.renee.hagood@gmail.com>
2019-11-19 11:43:06 -0500

    cleanup

--- app/interfaces/EmployeeRepositoryInterface.php
+++ app/interfaces/EmployeeRepositoryInterface.php
@@ -4,8 +4,6 @@ namespace App\Interfaces;
 
 interface EmployeeRepositoryInterface {
     public function find($id);
-    // public function validate($data);
-    // public function getErrors();
     public function create($data);
     public function update($data);
     public function delete($data);
